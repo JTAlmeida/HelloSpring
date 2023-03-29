6bf62548812bdc9e13a3fb96530d57120f942a36
Jonatas <jonatasalmeidasm@gmail.com>
2023-03-29 13:56:24 -0300

    fixed return string

--- src/main/java/com/teste/demo/controllers/helloController.java
+++ src/main/java/com/teste/demo/controllers/helloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloController {
     @GetMapping
     public String getHello() {
-        return "Olá Spring!";
+        return "Olá Spring";
     }
 }
